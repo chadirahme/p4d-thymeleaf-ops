9f51f079faf0b34f701b0a724f28a62d276807d0
chadirahme <eng.chadi@gmail.com>
2022-07-31 23:59:37 -0400

    init project

---
+++ src/main/java/com/p4d/ops/P4dThymeleafOpsApplication.java
@@ -0,0 +1,13 @@
+package com.p4d.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P4dThymeleafOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P4dThymeleafOpsApplication.class, args);
+	}
+
+}
